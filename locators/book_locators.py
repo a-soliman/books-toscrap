97de7d42898f14a480c1bb90eb0c6b29cc9c4d3a
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-02-28 08:22:19 -0800

    coded the locators

---
+++ locators/book_locators.py
@@ -0,0 +1,9 @@
+"""
+information from within a single book
+"""
+
+
+class BookLocators:
+    TITLE = "h3 a"
+    PRICE = "div.product_price p.price_color"
+    IN_STOCK = "div.product_price p.instock availability"
---
+++ locators/books_page_locators.py
@@ -0,0 +1,7 @@
+"""
+extracts books from a page
+"""
+
+
+class BooksPageLocators:
+    BOOK = "li.col-xs-6.col-sm-4.col-md-3.col-lg-3"
